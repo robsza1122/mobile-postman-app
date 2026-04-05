7f1f50308af28180950ccdb10c606de723da47fd
Robert <robert.szalapak@gmail.com>
2026-04-05 14:51:19 +0200

    changing yaml

--- backend/src/config/MobilePostmanDB.ts
+++ backend/src/config/MobilePostmanDB.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const postManDataBase = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_DB_URI || 'mongodb://localhost:27017/NEW_DATABASE_NAME');
+    await mongoose.connect(process.env.MONGO_DB_URI || MONGO_URI);
     console.log("Successfully connected with DB Atlas");
   } catch (error) {
     console.error("Cannot connect to DB", error);
