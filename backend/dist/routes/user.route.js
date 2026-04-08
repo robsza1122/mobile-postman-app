1321664dafdda81d60ede5991dfd40f4e430d04f
Robert <robert.szalapak@gmail.com>
2026-04-08 09:13:35 +0200

    connecting folders

--- backend/dist/routes/user.route.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_controller_1 = require("../controllers/user.controller");
-const userRoutes = (0, express_1.Router)();
-userRoutes.get("/", user_controller_1.getUserHandler);
-exports.default = userRoutes;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,8 +21,10 @@ app.use(express.json());
 app.use(urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: APP_ORIGIN,
+    origin: [APP_ORIGIN, "http://localhost:5173"],
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
   }),
 );
 app.use(cookieParser());
@@ -44,3 +46,4 @@ app.listen(PORT, async () => {
   console.log(`Server is listening on port ${PORT} in ${NODE_ENV}`);
   await postManDataBase();
 });
+app.use(express.json());
