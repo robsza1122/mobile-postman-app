23448d5623310dcbc995eac0cdbd3a0061dc0c44
Robert <robert.szalapak@gmail.com>
2026-04-10 06:55:07 +0200

    fixing types

--- backend/src/@types/express/index.d.ts
+++ backend/src/@types/express/index.d.ts
@@ -1,10 +1,8 @@
 import * as express from 'express';
 
-declare global {
-  namespace Express {
-    interface Request {
-      userId?: string;
-      sessionId?: string;
-    }
+declare namespace Express {
+  export interface Request {
+    userId?: string;
+    sessionId?: string;
   }
 }
